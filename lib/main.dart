50fdbed9a91b44fe148989d865645f4bd7f17e30
Mohammad1380H <hatmy418@gmail.com>
2022-11-12 23:16:52 +0330

    first settings

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -9,9 +10,31 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      title: 'TechBlog',
+      // ignore: prefer_const_literals_to_create_immutables
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      // ignore: prefer_const_literals_to_create_immutables
+      supportedLocales: [
+        const Locale('fa', ''), // English, no country code
+      ],
       theme: ThemeData(
-        primarySwatch: Colors.red,
+        textTheme: const TextTheme(
+            headline1: TextStyle(
+                fontFamily: "cinema", fontSize: 20, color: Colors.black),
+            headline2: TextStyle(
+                fontFamily: "cinema", fontSize: 20, color: Colors.white),
+            headline3: TextStyle(
+                fontFamily: "cinema", fontSize: 16, color: Colors.black),
+            bodyText1: TextStyle(
+                fontFamily: "homa", fontSize: 12, color: Colors.black),
+            bodyText2: TextStyle(
+                fontFamily: "homa", fontSize: 12, color: Colors.white)),
+        primarySwatch: Colors.purple,
       ),
       home: const Scaffold(),
     );
